99b92663076dddda59b50f506c4d1d61cbd19b9d
Danny <ddeneau@terpmail.umd.edu>
2022-06-30 15:52:30 -0400

    Got data to show up on the terminal
    Right now, there is a virtual memory and CPU section, without any controls.

--- go.mod
+++ go.mod
@@ -7,13 +7,20 @@ require github.com/gizak/termui/v3 v3.1.0
 require (
 	github.com/gdamore/encoding v1.0.0 // indirect
 	github.com/gdamore/tcell/v2 v2.4.1-0.20210905002822-f057f0a857a1 // indirect
+	github.com/go-ole/go-ole v1.2.6 // indirect
 	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
+	github.com/lufia/plan9stats v0.0.0-20211012122336-39d0f177ccd0 // indirect
 	github.com/mattn/go-runewidth v0.0.13 // indirect
 	github.com/mitchellh/go-wordwrap v0.0.0-20150314170334-ad45545899c7 // indirect
 	github.com/nsf/termbox-go v0.0.0-20190121233118-02980233997d // indirect
+	github.com/power-devops/perfstat v0.0.0-20210106213030-5aafc221ea8c // indirect
 	github.com/rivo/tview v0.0.0-20220610163003-691f46d6f500 // indirect
 	github.com/rivo/uniseg v0.2.0 // indirect
-	golang.org/x/sys v0.0.0-20210309074719-68d13333faf2 // indirect
+	github.com/shirou/gopsutil/v3 v3.22.5 // indirect
+	github.com/tklauser/go-sysconf v0.3.10 // indirect
+	github.com/tklauser/numcpus v0.4.0 // indirect
+	github.com/yusufpapurcu/wmi v1.2.2 // indirect
+	golang.org/x/sys v0.0.0-20220128215802-99c3d69c2c27 // indirect
 	golang.org/x/term v0.0.0-20210220032956-6a3ed077a48d // indirect
 	golang.org/x/text v0.3.6 // indirect
 )
--- main.go
+++ main.go
@@ -1,22 +1,48 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/rivo/tview"
+	"github.com/shirou/gopsutil/v3/cpu"
+	"github.com/shirou/gopsutil/v3/mem"
 )
 
 func main() {
-	getSystemData()
-	box := tview.NewBox().SetBorder(true).SetTitle("Hello, world!")
-	if err := tview.NewApplication().SetRoot(box, true).Run(); err != nil {
+
+	app := tview.NewApplication()
+
+	total, used, available := getVirtualMemory()
+	cpu := getCPU()
+
+	memorySummary := fmt.Sprintf("total: %d, Used: %d, Available: %d", total, used, available)
+	cpuSummary := fmt.Sprintf("CPU: %s", cpu)
+
+	memoryText := tview.NewTextView().SetText(memorySummary)
+	memoryText.SetTitle("Memory:")
+	cpuText := tview.NewTextView().SetText(cpuSummary)
+	cpuText.SetTitle("CPU:")
+
+
+	grid := tview.NewGrid().AddItem(memoryText, 0, 0, 2, 2, 2, 2, false)
+	grid.AddItem(cpuText, 1, 0, 2, 2, 2, 2, false)
+
+	if err := app.SetRoot(grid, true).Run(); err != nil {
 		panic(err)
 	}
 
 }
 
-func getSystemData() {
+func getVirtualMemory() (int64, int64, int64) {
+	v, _ := mem.VirtualMemory()
 
+	return int64(v.Total), int64(v.Used), int64(v.Available)
 }
 
-/* numberVersions - int - the number of project versions being fed into the test.
+func getCPU() string {
+	v, _ := cpu.Info()
 
- */
+	cpuInfo := cpu.InfoStat(v[0])
+
+	return cpuInfo.Family
+}
