406f00e164de331874f1fedbb684f06ad3c90266
Danny <ddeneau@terpmail.umd.edu>
2022-07-09 20:36:58 -0400

    Moved out backend API calls from the frontend script.
    Now, main.go should only handle GUI set-up and updates, while all the actual system information data collection is handled in systemInformation.go

--- main.go
+++ main.go
@@ -1,25 +1,23 @@
 package main
 
 import (
-	"fmt"
 	"time"
 
 	"github.com/gdamore/tcell/v2"
 	"github.com/rivo/tview"
-	"github.com/shirou/gopsutil/v3/cpu"
-	"github.com/shirou/gopsutil/v3/mem"
 )
 
 const refreshInterval = 1 * time.Millisecond // Used for application loop
-type terminalGUI struct {
-	app  *tview.Application
-	grid *tview.Grid
-}
+const ui = *terminalGUI
 
-var (
-	memoryTV *tview.TreeView
-	cpuTV    *tview.TreeView
-)
+type terminalGUI struct {
+	app        *tview.Application
+	grid       *tview.Grid
+	memoryView *tview.Box
+	cpuView    *tview.Box
+	memText    *tview.TextView
+	cpuText    *tview.TextView
+}
 
 /* Define structure for each type of system info read-out (modularize CPU, Memory stuff, out of main)*/
 
@@ -27,64 +25,33 @@ func main() {
 
 	ui := initialzeUI()
 
-	memoryTV := tview.NewTreeView() // Don't use treeview here...probably not the best idea.
-	memoryTV.SetBackgroundColor(tcell.Color102)
-	memoryTV.SetBorder(true).SetTitle("RAM")
-	cpuTV := tview.NewTreeView()
-
-	cpuCoreTV := tview.NewTextView()
-	cpuTV.SetBackgroundColor(tcell.Color102)
-	cpuCoreTV.SetText(getCPU())
-	cpuTV.SetBorder(true).SetTitle("CPU")
-
-	ui.grid = tview.NewGrid().AddItem(memoryTV, 0, 0, 1, 1, 1, 1, false)
-	ui.grid.AddItem(cpuTV, 1, 0, 1, 1, 1, 1, false)
-
 	if err := ui.app.SetRoot(ui.grid, true).Run(); err != nil {
 		panic(err)
 	}
 
-	go updateTime(ui.app)
+	updateUI()
 
 }
 
-/* Start-up the UI with basic components*/
+/* Start-up the UI with basic components (two basic components for now, lol)*/
 func initialzeUI() terminalGUI {
-	ui := terminalGUI{tview.NewApplication(), tview.NewGrid()}
+	app, grid := tview.NewApplication(), tview.NewGrid()
+	memoryView, cpuView := tview.NewBox(), tview.NewBox()
+	memText, cpuText := tview.NewTextView(), tview.NewTextView()
+
+	memoryView.SetBackgroundColor(tcell.Color102)
+	memoryView.SetBorder(true).SetTitle("RAM")
+
+	cpuView.SetBackgroundColor(tcell.Color102)
+	cpuView.SetBorder(true).SetTitle("CPU")
+
+	ui := terminalGUI{app, grid, memoryView, cpuView, memText, cpuText}
+
 	return ui
 }
 
-func updateTime(app *tview.Application) {
-	for {
-		time.Sleep(refreshInterval)
-
-	}
-}
-
 func updateUI() {
+	sysInfo := getCPU()
+	ui.cpuText.SetText(sysInfo)
 
-}
-
-/* Get the virual memory */
-func getVirtualMemory() string {
-	v, _ := mem.VirtualMemory()
-
-	total, used, available := v.Total, v.Used, v.Available
-
-	memorySummary := fmt.Sprintf("total: %d mb, \n Used: %d mb, \n Available: %d mb", total, used, available)
-
-	return memorySummary
-}
-
-/* Get # cores and percentage used */
-func getCPU() string {
-	v, _ := cpu.Info()
-
-	coreTime, _ := cpu.Percent(0, false)
-
-	cpuInfo := cpu.InfoStat(v[0])
-
-	cpuSummary := fmt.Sprintf("Cores: %d  Used: %f", cpuInfo.Cores, coreTime[0])
-
-	return cpuSummary
 }
