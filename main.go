296ea41ba286a65e52aa6f0feaa3ce537a8238c0
Danny <ddeneau@terpmail.umd.edu>
2022-07-11 12:54:02 -0400

    Fixed some formatting and code issues.
    Mostly removed unused variables and added a temporary timer for debugging the looping.

--- main.go
+++ main.go
@@ -1,18 +1,22 @@
 package main
 
 import (
+	"fmt"
+	"time"
+
 	"github.com/gdamore/tcell/v2"
 	"github.com/rivo/tview"
 )
+
 type terminalGUI struct {
-	app        *tview.Application
-	grid       *tview.Grid
-	memoryView *tview.Box
-	cpuView    *tview.Box
-	memText    *tview.TextView
-	cpuText    *tview.TextView
+	app       *tview.Application
+	grid      *tview.Grid
+	memText   *tview.TextView
+	cpuText   *tview.TextView
+	tempTimer *tview.TextView
 }
 
+var stopped = false
 
 /* Define structure for each type of system info read-out (modularize CPU, Memory stuff, out of main)*/
 
@@ -20,33 +24,48 @@ func main() {
 
 	ui := initialzeUI()
 
-	if err := ui.app.SetRoot(ui.grid, true).Run(); err != nil {
-		panic(err)
-	}
+	ui.app.SetInputCapture(func(event *tcell.EventKey) *tcell.EventKey {
+		if event.Key() == tcell.KeyEscape {
+			stopped = true
+			ui.app.Stop()
+		}
+		return event
+	})
 
-	updateUI(&ui)
+	for !stopped {
+		updateUI(&ui)
+		time.Sleep(time.Second)
+
+		if err := ui.app.SetRoot(ui.grid, true).Run(); err != nil {
+			panic(err)
+		}
+	}
 
 }
 
 /* Start-up the UI with basic components (two basic components for now, lol)*/
 func initialzeUI() terminalGUI {
 	app, grid := tview.NewApplication(), tview.NewGrid()
-	memoryView, cpuView := tview.NewBox(), tview.NewBox()
-	memText, cpuText := tview.NewTextView(), tview.NewTextView()
+	memText, cpuText, tt := tview.NewTextView(), tview.NewTextView(), tview.NewTextView()
 
-	memoryView.SetBackgroundColor(tcell.Color102)
-	memoryView.SetBorder(true).SetTitle("RAM")
+	memText.SetBackgroundColor(tcell.Color102)
+	memText.SetBorder(true).SetTitle("RAM")
 
-	cpuView.SetBackgroundColor(tcell.Color102)
-	cpuView.SetBorder(true).SetTitle("CPU")
+	cpuText.SetBackgroundColor(tcell.Color102)
+	cpuText.SetBorder(true).SetTitle("CPU")
 
-	ui := terminalGUI{app, grid, memoryView, cpuView, memText, cpuText}
+	ui := terminalGUI{app, grid, memText, cpuText, tt}
 
 	return ui
 }
 
 func updateUI(ui *terminalGUI) {
-	sysInfo := [2]string {getCPU(), getVirtualMemory()}
+	sysInfo := [2]string{getCPU(), getVirtualMemory()}
 	ui.cpuText.SetText(sysInfo[0])
 	ui.memText.SetText(sysInfo[1])
+	ui.tempTimer.SetText(fmt.Sprintf("%d", time.Now().Unix()))
+	ui.grid.AddItem(ui.cpuText, 0, 0, 2, 1, 1, 1, true)
+	ui.grid.AddItem(ui.memText, 0, 1, 2, 1, 1, 1, false)
+	ui.grid.AddItem(ui.tempTimer, 2, 1, 1, 2, 1, 2, false)
+
 }
